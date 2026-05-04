da36d288a5d399c0d4100512bd8aa756cbf3ad8e
Leila <galiul1naaal@gmail.com>
2026-05-05 03:27:08 +0600

    add ingredients data

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+import meat from './assets/meat.jpg';
+import cheese from './assets/cheese.jpg';
+import salad from './assets/salad.jpg';
+import bacon from './assets/bacon.jpg';
+
+export const INGREDIENTS = [
+    {name: 'Meat', price: 80, image: meat},
+    {name: 'Cheese', price: 50, image: cheese},
+    {name: 'Salad', price: 10, image: salad},
+    {name: 'Bacon', price: 60, image: bacon},
+];
